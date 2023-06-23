1a984a35b59ffb5c17e79da6233f2a145a68c5b2
Darya Krauchenia <darya.krauchenia@gmail.com>
2023-06-23 20:46:32 +0200

    Adding CSharpOOP3 project

---
+++ CSharpOOP3/CSharpOOP3/Program.cs
@@ -0,0 +1,21 @@
+﻿using CSharpOOP3;
+
+static class Program
+{
+    static void Main()
+    {
+        //Task 2. Operation with properties from Task 1
+        Scooter speedRun = new Scooter();
+        //speedRun.Model = "Upgraded Speed Runner"; //Error line
+        speedRun.Speed = 25;
+
+        Console.WriteLine($"The scooter model is {speedRun.Model}");
+        //Console.WriteLine($"The scooter speed is {speedRun.Speed}"); //Error line
+
+        Scooter.EngineType = "Electric"; //Task 4. Adding value to a static field
+        Scooter.DisplayType(); //Task 5. Calling a static method
+
+        ScooterHelper.CountPrice(speedRun); //Task 7. Calling static methods from a static class
+        ScooterHelper.SetUsage(speedRun);
+    }
+}
---
+++ CSharpOOP3/CSharpOOP3/Scooter.cs
@@ -0,0 +1,44 @@
+﻿namespace CSharpOOP3
+{
+    public class Scooter
+    {
+        public string Model { get; } = "SpeedRunner"; //Task 1. Creating properties. Property for reading only
+
+        private double _speed;
+
+        public double Speed //Task 1. Property for being assigned
+        {
+            set
+            {
+                _speed = value;
+            }
+        }
+
+        private bool _isAvailable; //Task 3. Adding accessors to the field
+
+        public bool IsAvailable
+        {
+            get 
+            { 
+                return _isAvailable; 
+            }
+
+            private set 
+            { 
+                _isAvailable = value; 
+            }
+        }
+
+        public double Price; //Fields for operations in the helper class
+        public bool IsUsed;
+
+        public static string EngineType { get; set; } //Task 4. Static field
+
+        public static void DisplayType() //Task 5. Static method
+        {
+            Console.WriteLine($"The type of scooter is: {EngineType}");
+        }
+
+
+    }
+}
