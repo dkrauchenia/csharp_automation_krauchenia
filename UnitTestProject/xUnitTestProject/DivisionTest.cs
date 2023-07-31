2d6fbbdf1f8bde116b60272afebd819592b98f85
Darya Krauchenia <darya.krauchenia@gmail.com>
2023-07-31 21:29:42 +0200

    Adding UnitTestProject

---
+++ UnitTestProject/xUnitTestProject/DivisionTest.cs
@@ -0,0 +1,36 @@
+﻿using CSharpCalculator;
+
+namespace xUnitTestProject
+{
+    public class DivisionTest
+    {
+        private Calculator calculator;
+
+        public DivisionTest()
+        {
+            calculator = new Calculator();
+        }
+
+        [Theory]
+        [InlineData(8.0, 4.0, 2.0)]
+        [InlineData(-6.0, -2.0, 3.0)]
+        [InlineData(0.0, 10.0, 0.0)]
+        public void Divide_ReturnCorrectResult(double a, double b, double expected)
+        {
+            Calculator calculator = new Calculator();
+            double result = calculator.Divide(a, b);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(8.0, double.NaN)]
+        [InlineData(double.NaN, -3)]
+        public void Divide_ReturnNaN(double a, double b)
+        {
+            Calculator calculator = new Calculator();
+            double result = calculator.Divide(a, b);
+            Assert.True(double.IsNaN(result));
+        }
+        
+    }
+}
---
+++ UnitTestProject/xUnitTestProject/SquareRootTest.cs
@@ -0,0 +1,37 @@
+﻿using CSharpCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xUnitTestProject
+{
+    public class SquareRootTest
+    {
+        private Calculator calculator;
+
+        public SquareRootTest()
+        {
+            calculator = new Calculator();
+        }
+
+        [Theory]
+        [InlineData(4, 2)]
+        [InlineData(16, 4)]
+        [InlineData(0, 0)]
+        [InlineData(1, 1)]
+        public void Sqrt_ReturnCorrectResult(int number, int expected)
+        {
+            var result = calculator.Sqrt(number);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("invalid_input")]
+        public void Sqrt_ReturnException(object a)
+        {
+            Assert.Throws<FormatException>(() => calculator.Sqrt(a));
+        }
+    }
+}
