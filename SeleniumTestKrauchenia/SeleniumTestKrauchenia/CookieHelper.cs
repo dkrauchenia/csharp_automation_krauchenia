cd9a83aaf735422e6c8ec3e95c667899592faf98
Darya Krauchenia <darya.krauchenia@gmail.com>
2023-08-22 19:53:16 +0200

    Adding Selenium_Test (task 3 and 5)

---
+++ SeleniumTestKrauchenia/SeleniumTestKrauchenia/CookieHelper.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTestKrauchenia
+{
+    public class CookieHelper
+    {
+        private IWebDriver _driver;
+
+        public CookieHelper(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public void AcceptCookies()
+        {
+            var acceptButton = _driver.FindElement(By.Id("onetrust-accept-btn-handler"));
+            acceptButton.Click();
+
+        }
+    }
+}
---
+++ SeleniumTestKrauchenia/SeleniumTestKrauchenia/HeaderTests.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTestKrauchenia
+{
+    [TestFixture]
+    public class HeaderTests
+    {
+        private IWebDriver _driver { get; set; }
+        private const string _epamHomePageUrl = "https://www.epam.com/";
+        public CookieHelper _cookieHelper { get; set; }
+
+        [SetUp]
+        public void SetupBrowser()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+            _driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(10);
+            _driver.Navigate().GoToUrl(_epamHomePageUrl);
+            _cookieHelper = new CookieHelper(_driver);
+            _cookieHelper.AcceptCookies();
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            _driver.Quit();
+        }
+
+        [Test]
+        public void HeaderElementIsPresent()
+        {
+            IWebElement headerElement = _driver.FindElement(By.ClassName("header__content"));
+
+            Assert.IsNotNull(headerElement, "Header should be present on the page");
+        }
+    }
+}
